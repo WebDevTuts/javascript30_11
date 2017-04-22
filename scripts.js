e5e871dc7a9e36d45c6b452823221c9ee5d48558
Edwin Chen <edwinchen85@gmail.com>
2017-04-22 21:43:17 +0800

    Create skip func and hook up to event listener

--- scripts.js
+++ scripts.js
@@ -18,9 +18,15 @@ function updateButton() {
   toggle.textContent = icon;
 }
 
+function skip() {
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
 /* Hook up event listener */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
